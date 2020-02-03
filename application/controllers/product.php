153e1a3f41c28191fc0782a7950da1b8c0a92105
kemcheawchan <54612816+kemcheawchan@users.noreply.github.com>
2020-02-03 14:23:50 +0700

    เครื่องพง

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -3,15 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Product extends CI_Controller {
 
+	public function __construct()
+	{
+ 		parent::__construct();
+		$this->load->model('adminshop_model');
+	}
+
 
 	public function index()
 	{
+			$data['query'] = $this->adminshop_model->jointableshop();
 		$this->load->view('header_view');
 		$this->load->view('css');
 		$this->load->view('banner');
-		$this->load->view('product');
+		$this->load->view('product',$data);
 		$this->load->view('footer');
 		$this->load->view('js');
 	}
+
 }
 ?>
--- application/views/product.php
+++ application/views/product.php
@@ -49,11 +49,13 @@
                         <li>Coffee Equipment</li>
                     </ul>
                 </div>
+
                 <div class="product-slider owl-carousel">
+                  <?php foreach ($query as $row) {?>
                     <div class="product-item">
                         <div class="pi-pic">
-                            <img src="<?php echo base_url('assets/img/products'); ?>/Hot coffee2.jpg" alt="">
-                            <div class="sale">Sale</div>
+                            <img src="<?php echo base_url('assets/img/uploadimg'); ?>/<?php echo $row->product_img; ?>" height="250px" width="600px">
+
                             <div class="icon">
                                 <i class="icon_heart_alt"></i>
                             </div>
@@ -64,88 +66,25 @@
                             </ul>
                         </div>
                         <div class="pi-text">
-                            <div class="catagory-name">Coffee Drink</div>
+                            <div class="catagory-name"><?php echo $row->product_type_name; ?></div>
                             <a href="#">
-                                <h5>Americano</h5>
+                                <h5><?php echo $row->product_name; ?></h5>
                             </a>
                             <div class="product-price">
-                                ฿40.00
-                                <span>฿35.00</span>
+                              ฿<?php echo $row->product_price; ?>.00
+
                             </div>
                         </div>
                     </div>
-                    <div class="product-item">
-                        <div class="pi-pic">
-                            <img src="<?php echo base_url('assets/img/products'); ?>/Hot coffee4.jpg" alt="">
-                            <div class="icon">
-                                <i class="icon_heart_alt"></i>
-                            </div>
-                            <ul>
-                                <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="pi-text">
-                            <div class="catagory-name">Coffee Drink</div>
-                            <a href="#">
-                                <h5>Caffe latte</h5>
-                            </a>
-                            <div class="product-price">
-                                ฿50.00
-                            </div>
-                        </div>
-                    </div>
-                    <div class="product-item">
-                        <div class="pi-pic">
-                            <img src="<?php echo base_url('assets/img/products'); ?>/Hot coffee5.jpg" alt="">
-                            <div class="icon">
-                                <i class="icon_heart_alt"></i>
-                            </div>
-                            <ul>
-                                <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="pi-text">
-                            <div class="catagory-name">Coffee Drink</div>
-                            <a href="#">
-                                <h5>Caffe Mocha</h5>
-                            </a>
-                            <div class="product-price">
-                                ฿50.00
-                            </div>
-                        </div>
-                    </div>
-                    <div class="product-item">
-                        <div class="pi-pic">
-                            <img src="<?php echo base_url('assets/img/products'); ?>/Hot coffee3.jpg" alt="">
-                            <div class="icon">
-                                <i class="icon_heart_alt"></i>
-                            </div>
-                            <ul>
-                                <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="pi-text">
-                            <div class="catagory-name">Coffee Drink</div>
-                            <a href="#">
-                                <h5>Cappuccino</h5>
-                            </a>
-                            <div class="product-price">
-                                ฿50.00
-                            </div>
-                        </div>
-                    </div>
+                    <?php } ;?>
+
                 </div>
             </div>
         </div>
     </div>
 </section>
 <!-- Women Banner Section End -->
+
 
 <!-- Man Banner Section Begin -->
 <section class="man-banner spad">
@@ -262,3 +201,57 @@
     </div>
 </section>
 <!-- Man Banner Section End -->
+
+<!-- Women Banner Section Begin -->
+<section class="women-banner spad">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="product-large set-bg" data-setbg="<?php echo base_url('assets/img/index'); ?>/Bg7.jpg">
+                    <h2>Coffee Equipment</h2>
+                    <a href="#">Discover More</a>
+                </div>
+            </div>
+            <div class="col-lg-8 offset-lg-1">
+                <div class="filter-control">
+                    <ul>
+                        <li class="active">Coffee Drink</li>
+                        <li>Coffee Beans</li>
+                        <li>Coffee Equipment</li>
+                    </ul>
+                </div>
+
+                <div class="product-slider owl-carousel">
+                  <?php foreach ($query as $row) {?>
+                    <div class="product-item">
+                        <div class="pi-pic">
+                            <img src="<?php echo base_url('assets/img/uploadimg'); ?>/<?php echo $row->product_img; ?>" height="250px" width="600px">
+
+                            <div class="icon">
+                                <i class="icon_heart_alt"></i>
+                            </div>
+                            <ul>
+                                <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
+                                <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="pi-text">
+                            <div class="catagory-name"><?php echo $row->product_type_id; ?></div>
+                            <a href="#">
+                                <h5><?php echo $row->product_name; ?></h5>
+                            </a>
+                            <div class="product-price">
+                              ฿<?php echo $row->product_price; ?>.00
+
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ;?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Women Banner Section End -->

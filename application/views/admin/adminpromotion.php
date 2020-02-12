d228c2c2cb81edb2e1173059b885e50cbf89a69a
pongsatom <59642556+pongsatom@users.noreply.github.com>
2020-02-12 16:42:23 +0700

    เก็บรายละเอียดหน้าหลังบ้าน 2/12/2020

--- application/views/admin/adminpromotion.php
+++ application/views/admin/adminpromotion.php
@@ -2,23 +2,23 @@
 <div class="row">
     <div class="col-md-12">
         <!-- DATA TABLE -->
-        <h3 class="title-5 m-b-35">จัดการข้อมูลโปรโมชั่น</h3>
         <div class="table-data__tool">
+          <h3 class="title-5 m-b-35">จัดการข้อมูลโปรโมชั่น</h3>
             <div class="table-data__tool-left">
                 <div class="rs-select2--light rs-select2--md">
-                    <select class="js-select2" name="property">
+                    <!-- <select class="js-select2" name="property">
                         <option selected="selected">All Properties</option>
                         <option value="">Option 1</option>
                         <option value="">Option 2</option>
-                    </select>
+                    </select> -->
                     <div class="dropDownSelect2"></div>
                 </div>
                 <div class="rs-select2--light rs-select2--sm">
-                    <select class="js-select2" name="time">
+                    <!-- <select class="js-select2" name="time">
                         <option selected="selected">Today</option>
                         <option value="">3 Days</option>
                         <option value="">1 Week</option>
-                    </select>
+                    </select> -->
                     <div class="dropDownSelect2"></div>
                 </div>
             </div>

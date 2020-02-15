62cfeee949f32e99c2ef2a122f97f0595ddb33c9
pongsatom <59642556+pongsatom@users.noreply.github.com>
2020-02-15 13:25:10 +0700

    หน้า Header

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -11,11 +11,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="keywords" content="Fashi, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fashi</title>
+    <title>Salt & Light</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
-    
+
 </head>
